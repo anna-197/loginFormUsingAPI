15f02d67ab27fc0014b5e96e47d61c2ed1814c83
Ananya Nair <an.nair1702@gmail.com>
2023-01-01 20:45:47 +0530

    Initial commit using states to store data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleApi = () => {
+    console.log({ email, password })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Email: <input type="text" value={email} onChange={handleEmail} />
+      Password: <input type="text"  value={password} onChange={handlePassword} />
+      <button onClick={handleApi}> Submit </button>
     </div>
   );
 }
